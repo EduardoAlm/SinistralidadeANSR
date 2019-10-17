4ff48f3a4402cb9725d28191f5b7fafbe6e2870e
Carolina Lopes <carolinagalvaolopes@gmail.com>
2019-10-17 23:15:47 +0100

    DB connection

--- SinistralidadeBackend/backend/sinistralidade/models.py
+++ SinistralidadeBackend/backend/sinistralidade/models.py
@@ -5,3 +5,4 @@ class User(models.Model):
     nr_cc = models.IntegerField(primary_key=True, editable=True)
     username = models.TextField(max_length=50)
     email = models.TextField(max_length=50)
+    distrito = models.TextField(max_length=150)
