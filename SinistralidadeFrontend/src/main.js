1fce4f54885bd1672b956a9166efd8e7935009bd
Eduardo <eduardorodalm95@gmail.com>
2019-10-22 16:56:24 +0100

    Part4 Login and registry Ready

--- SinistralidadeFrontend/src/main.js
+++ SinistralidadeFrontend/src/main.js
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-new Vue({
+const v = new Vue({
   router,
   store: store,
   render: h => h(App)
 }).$mount("#app");
+
+v.$store.dispatch("getLogin");
+v.$store.dispatch("getRegistry");
--- SinistralidadeFrontend/src/store.js
+++ SinistralidadeFrontend/src/store.js
@@ -1,42 +1,78 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import api from "./api/api.js";
 
 Vue.use(Vuex); // only required if you're using modules.
 // We're using modules, so there you go.
+const apiRoot = "http://localhost:8000";
 
 const store = new Vuex.Store({
   state: {
-    login: [{ text: "John Doe" }],
-    registry: [{}]
+    login: [],
+    registry: []
   },
   mutations: {
-    ADD_LOGIN: function(state, log) {
-      state.login.push(log);
+    GET_LOGIN: function(state, response) {
+      state.login = response.body;
+    },
+    ADD_LOGIN: function(state, response) {
+      state.login.push(response.body);
     },
     CLEAR_LOGIN: function(state) {
       const login = state.login;
       login.splice(0, login.length);
     },
-    ADD_REGISTRY: function(state, reg) {
-      state.registry.push(reg);
+    GET_REGISTRY: function(state, response) {
+      state.resgistry = response.body;
+    },
+    ADD_REGISTRY: function(state, response) {
+      state.registry.push(response.body);
     },
     CLEAR_REGISTRY: function(state) {
       const registry = state.registry;
       registry.splice(0, registry.length);
+    },
+    // Note that we added one more for logging out errors.
+    API_FAIL: function(state, error) {
+      console.error(error);
     }
   },
   actions: {
+    getLogin(store) {
+      return api
+        .get(apiRoot + "/login/")
+        .then(response => store.commit("GET_LOGIN", response))
+        .catch(error => store.commit("API_FAIL", error));
+    },
     addLogin(store, log) {
-      store.commit("ADD_LOGIN", log);
+      return api
+        .post(apiRoot + "/login/", log)
+        .then(response => store.commit("ADD_LOGIN", response))
+        .catch(error => store.commit("API_FAIL", error));
     },
     clearLogin(store) {
-      store.commit("CLEAR_LOGIN");
+      return api
+        .delete(apiRoot + "/login/clear_login/")
+        .then(response => store.commit("CLEAR_LOGIN", response))
+        .catch(error => store.commit("API_FAIL", error));
+    },
+    getRegistry(store) {
+      return api
+        .get(apiRoot + "/registry/")
+        .then(response => store.commit("GET_REGISTRY", response))
+        .catch(error => store.commit("API_FAIL", error));
     },
     addRegistry(store, reg) {
-      store.commit("ADD_REGISTRY", reg);
+      return api
+        .post(apiRoot + "/registry/", reg)
+        .then(response => store.commit("ADD_REGISTRY", response))
+        .catch(error => store.commit("API_FAIL", error));
     },
     clearRegistry(store) {
-      store.commit("CLEAR_REGISTRY");
+      return api
+        .delete(apiRoot + "/registry/clear_registry/")
+        .then(response => store.commit("CLEAR_REGISTRY", response))
+        .catch(error => store.commit("API_FAIL", error));
     }
   }
 });
